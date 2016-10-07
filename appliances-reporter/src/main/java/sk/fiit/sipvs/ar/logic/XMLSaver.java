2f9498ae66574ee1fcb9dac89b046deb356d4e3f
Jozef Zaťko <jozefzatko.sk@gmail.com>
2016-10-07 19:39:54 +0200

    JAXB Maven setup + prepared marshal operation

--- appliances-reporter/src/main/java/sk/fiit/sipvs/ar/logic/XMLSaver.java
+++ appliances-reporter/src/main/java/sk/fiit/sipvs/ar/logic/XMLSaver.java
@@ -1,5 +1,18 @@
 package sk.fiit.sipvs.ar.logic;
 
+import java.io.File;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import sk.fiit.sipvs.ar.report.*;
+
 /**
  * Saves data from Swing form into XML file
  *
@@ -10,6 +23,46 @@ public class XMLSaver implements Runnable {
 		
 		// TODO: Save form data to XML
 		System.out.println("Saving to XML...");
+		
+//		ApplianceReport report = new ObjectFactory().createApplianceReport();
+//		saveActualDate(report);
+//		saveToXMLFile(report, "file.xml");
 	}
 	
+	/**
+	 * Save ApplianceReport object into well-formed XML file
+	 * 
+	 * @param report
+	 * @param filename
+	 * @throws JAXBException
+	 */
+	private void saveToXMLFile(ApplianceReport report, String filename) throws JAXBException {
+		
+		JAXBContext jaxbContext = JAXBContext.newInstance("sk.fiit.sipvs.ar.report");
+		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+		File file = new File(filename);
+		
+		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		jaxbMarshaller.marshal(report, file);
+	}
+	
+	/**
+	 * Add actual date into ApplianceReport object
+	 * 
+	 * @param report
+	 */
+	private void saveActualDate(ApplianceReport report) {
+		
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(new Date());
+		
+		XMLGregorianCalendar date = null;
+		try {
+			date = DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
+		} catch (DatatypeConfigurationException e) {
+			e.printStackTrace();
+		}
+		report.setDate(date);
+	}
 }
