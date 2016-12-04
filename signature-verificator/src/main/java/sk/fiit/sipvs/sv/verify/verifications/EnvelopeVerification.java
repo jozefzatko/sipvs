c9c32d403c3abcb8c24a9ae83c4872c2e4de2bca
Jozef Zaťko <jozefzatko.sk@gmail.com>
2016-12-04 21:59:47 +0100

    EnvelopeVerification.verifyRootElement()

--- signature-verificator/src/main/java/sk/fiit/sipvs/sv/verify/verifications/EnvelopeVerification.java
+++ signature-verificator/src/main/java/sk/fiit/sipvs/sv/verify/verifications/EnvelopeVerification.java
@@ -1,6 +1,7 @@
 package sk.fiit.sipvs.sv.verify.verifications;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import sk.fiit.sipvs.sv.verify.DocumentVerificationException;
 
@@ -17,6 +18,20 @@ public class EnvelopeVerification extends Verification {
 	 * Koreňový element musí obsahovať atribúty xmlns:xzep a xmlns:ds podľa profilu XADES_ZEP
 	 */
 	public boolean verifyRootElement() throws DocumentVerificationException {
+		
+		Element envelope = document.getDocumentElement();
+		
+		if (assertElementAttributeValue(envelope, "xmlns:xzep", "http://www.ditec.sk/ep/signature_formats/xades_zep/v1.0") == false) {
+			
+			throw new DocumentVerificationException(
+					"Atribút xmlns:xzep koreňového elementu neobsahuje hodnotu http://www.ditec.sk/ep/signature_formats/xades_zep/v1.0");
+		}
+		
+		if (assertElementAttributeValue(envelope, "xmlns:ds", "http://www.w3.org/2000/09/xmldsig#") == false) {
+			
+			throw new DocumentVerificationException(
+					"Atribút xmlns:ds koreňového elementu neobsahuje hodnotu http://www.w3.org/2000/09/xmldsig#");
+		}
 		
 		return true;
 	}
--- signature-verificator/src/main/java/sk/fiit/sipvs/sv/verify/verifications/Verification.java
+++ signature-verificator/src/main/java/sk/fiit/sipvs/sv/verify/verifications/Verification.java
@@ -1,6 +1,7 @@
 package sk.fiit.sipvs.sv.verify.verifications;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Abstract Verification class
@@ -14,5 +15,17 @@ public abstract class Verification {
 	public Verification(Document document) {
 		
 		this.document = document;
+	}
+	
+	protected boolean assertElementAttributeValue(Element element, String attribute, String expectedValue) {
+		
+		String actualValue = element.getAttribute(attribute);
+		
+		if (actualValue != null && actualValue.equals(expectedValue)) {
+			
+			return true;
+			
+		}
+		return false;
 	}
 }
